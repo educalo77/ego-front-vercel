3e0c67dc71113ef184c26e4e34b7ddbaf82d5967
educalo77 <educalo@outlook.com>
2021-01-16 13:27:16 -0300

    ok

--- client/src/Store/constants/constants.js
+++
@@ -1,5 +0,0 @@
-export const GET_MODELS = "GET_MODELS";
-export const GET_OTHER = "GET_OTHER";
-export const GET_OTHER2 = "GET_OTHER2";
-export const GET_ORDER = "GET_ORDER";
-export const GET_ONE_MODEL = "GET_ONE_MODEL";
