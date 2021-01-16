e413c15211706239d6df87830a38160f85d4fc78
educalo77 <educalo@outlook.com>
2021-01-16 15:07:57 -0300

    sumando home con filter

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
 import './Styles/custom.scss';
-// import Home from './Pages/Home';
-// import ModelDetail from './Pages/ModelDetail';
+import Home from './Pages/Home';
+import ModelDetail from './Pages/ModelDetail';
 import Footer from './Components/Footer';
 
 
@@ -13,8 +13,8 @@ function App() {
   return (
     <React.Fragment>
       <Navbar />
-      {/* <Route exact path="/" component={Home} />
-      <Route exact path="/model/:id?" component={ModelDetail} /> */}
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/model/:id?" component={ModelDetail} />  */}
       <Footer />
     </React.Fragment>
   );
