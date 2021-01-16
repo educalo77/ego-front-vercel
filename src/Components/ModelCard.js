2ccd256cd38a9d5d7ed17104657da96c6aebf751
educalo77 <educalo@outlook.com>
2021-01-16 16:01:54 -0300

    viendo como solucionar tema modelcard

--- src/Components/ModelCard.js
+++ src/Components/ModelCard.js
@@ -1,24 +1,24 @@
 //import CurrencyFormat from 'react-currency-format';
 //import { Link } from 'react-router-dom';
 import React from 'react';
-import {Col, Card} from 'react-bootstrap';
+//import {Col, Card} from 'react-bootstrap';
 
 export default function ModelCard({ data }) {
 
 //    const { id, model, img, year, price } = data;
     
     return (
-        <Col lg={3} md={6}>
-            <Card className="border-0">
-                {/* <Card.Body className="text-center">
-                    <Card.Title className="text-center">{model}</Card.Title>
-                    <span>{year} | <CurrencyFormat displayType="text" decimalSeparator={','} thousandSeparator={'.'} prefix={'$'} value={price} /></span>
-                    <Link to={'/model/'+id}>
-                        <img src={img} width="100%" alt={'Imagen de: ' + model} />
-                    </Link>
-                    <Link to={'/model/'+id} className="btn btn-dark btn-ego">Ver Modelo</Link>
-                </Card.Body> */}
-            </Card>
-        </Col>
+        // <Col lg={3} md={6}>
+        //     <Card className="border-0">
+        //         <Card.Body className="text-center">
+        //             <Card.Title className="text-center">{model}</Card.Title>
+        //             <span>{year} | <CurrencyFormat displayType="text" decimalSeparator={','} thousandSeparator={'.'} prefix={'$'} value={price} /></span>
+        //             <Link to={'/model/'+id}>
+        //                 <img src={img} width="100%" alt={'Imagen de: ' + model} />
+        //             </Link>
+        //             <Link to={'/model/'+id} className="btn btn-dark btn-ego">Ver Modelo</Link>
+        //         </Card.Body>
+        //     </Card>
+        // </Col>
     );
 }
