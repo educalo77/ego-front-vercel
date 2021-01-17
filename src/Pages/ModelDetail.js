6f5ddfca5b8e8102118df71e0ea4985707e2bc9d
educalo77 <educalo@outlook.com>
2021-01-17 14:37:07 -0300

    opcion de no model detail

--- src/Pages/ModelDetail.js
+++ src/Pages/ModelDetail.js
@@ -6,6 +6,7 @@ import Carousel2 from '../Components/Carousel';
 import OtherDetails from '../Components/OtherDetails';
 import { useDispatch } from "react-redux";
 import { getAllModels } from '../Store/actions/actions';
+import s from '../Styles/home.module.scss';
 
 function ModelDetail(){
     const { id } = useParams();
@@ -24,11 +25,21 @@ function ModelDetail(){
 
     return (
         <React.Fragment>
+            {id !== undefined ? 
+                (
+            <>
             <Container >
                 <Model id={id} />
             </Container>
             <Carousel2 id={id} />
             <OtherDetails id={id} />
+            </>
+
+            ) :
+                (
+            <h1 className={s['page-title-detail']}>Elija un modelo</h1>
+            )
+                }
         </React.Fragment>
     )
 }
